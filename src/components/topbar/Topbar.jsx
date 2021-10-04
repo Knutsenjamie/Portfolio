26a91e5cbac8299b48285bf4e0611ec5a611e881
“Jamie <knutsenjamie@yahoo.com>
2021-10-04 16:31:40 -0700

    add email, LinkedIn, and Github icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,6 +3,11 @@ import "./topbar.scss"
 import * as React from 'react';
 import { AppBar, Box, Toolbar, Typography, IconButton} from '@material-ui/core';
 import Menu from "@material-ui/icons/Menu";
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+
 
 
 
@@ -25,6 +30,24 @@ export default function Topbar() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Hello!  I'm Jamie :) 
                     </Typography>
+                    <IconButton 
+                        color="inherit"
+                        sx={{ml: 8}}
+                    >
+                        <LinkedInIcon/>
+                    </IconButton>
+                    <IconButton 
+                        color="inherit"
+                        sx={{ml: 8}}
+                    >
+                        <EmailIcon/>
+                    </IconButton>
+                    <IconButton 
+                        color="inherit"
+                        sx={{ml: 8}}
+                    >
+                        <GitHubIcon/>
+                    </IconButton>
                     </Toolbar>
                 </AppBar>
             </Box>
